edfdc4c50325db59d3978e2ea3dbc36dddb20637
Leonardo Omaña <leotangram@gmail.com>
2024-01-16 21:30:58 -0500

    state component

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
-  template: `
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js</p>
-  `
+  // template: `
+  //   <h1>Hola Mundo</h1>
+  //   <p>Desde app.js</p>
+  // `
+  // methods: {},
+  // watch: {},
+  // setup() {}
+  data() {
+    return {
+      message: 'Hola Mundo',
+      quote: "I'm Batman",
+      author: 'Bruce Wayne'
+    }
+  }
 })
 
 app.mount('#myApp')
